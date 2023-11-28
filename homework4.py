14a4da8ec0ea6db4f3e75303e3b1559603dd8f77
minchev1401 <vvasev020@gmail.com>
2023-11-28 11:25:55 +0200

    1

---
+++ homework4.py
@@ -0,0 +1,24 @@
+
+print("Принтиране на 1, 4 и 7 буква от име!")
+
+my_string = "Jordans"
+result = my_string[0] + my_string[3] + my_string[-1]
+
+print(result)
+
+#
+
+print("Принтиране на 1, 4 и 7 буква от име!")
+
+my_string = "Jordans"
+
+split1 = my_string[:3]
+split2 = my_string[3]
+split3 = my_string[5:]
+
+first_letter = split1[0]
+fourth_letter = split2
+last_letter_ = split3[1]
+
+print(f"{first_letter}{fourth_letter}{last_letter_}")
+
